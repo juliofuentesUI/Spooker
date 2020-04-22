aa180909c7e4570af7dea1b4746f3b68b7b4111c
juliofuentes408 <juliofuentes@outlook.com>
2020-04-21 20:25:51 -0700

    Created Mock Data endpoint for featured clothes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const MOCK_DATA = require('./MockData.json');
+
+console.log(MOCK_DATA);
 
 const PORT = 8080;
 
-// app.use('/', express.static(path.join(__dirname, '/../dist')));
+app.use('/', express.static(path.join(__dirname, '/../dist')));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/../dist/index.html'));
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../dist/index.html'));
+});
+
+app.get('/featuredClothes', (req, res) => {
+    res.status(200).send(MOCK_DATA); 
+});
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
