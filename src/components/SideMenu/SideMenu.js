31b4c1313f4a2c13b516b86e24282f9ebb7ae5aa
juliofuentes408 <juliofuentes@outlook.com>
2020-05-01 22:17:58 -0700

    Implement dynamic css active class for collapsing hamburger Menu effect

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -4,14 +4,19 @@ import classes from './SideMenu.module.css';
 const SideMenu = (props) => {
 
     const [menuClicked, toggleMenu] = useState(false);
+    const inputClasses = [classes.headerMenuWrap];
+
+    if (menuClicked) {
+        inputClasses.push(classes.active);
+    }
 
     const toggleSideMenu = () => {
-        
+        toggleMenu(!menuClicked);
     };
 
 
     return (
-        <div onClick={} className={classes.headerMenuWrap}>
+        <div onClick={toggleSideMenu} className={inputClasses.join(' ')}>
             <span></span>
         </div>
     );
