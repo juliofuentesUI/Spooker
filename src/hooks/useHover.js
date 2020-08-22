60bd6caff877a0d1ba263b4be64d0aade6fac36f
Julio Fuentes <juliofuentes408@gmail.com>
2020-08-22 12:50:38 -0700

    Added custom test hook for hoverState effects to be shared amongst component

---
+++ src/hooks/useHover.js
@@ -0,0 +1,28 @@
+import React, { useRef, useState, useEffect} from 'react';
+
+const useHover = () => {
+  const [hovered, setHover] = useState(true);
+  const myRef = useRef(null);
+
+  const handleMouseOver = () => setHover(true);
+  const handleMouseOut = () => setHover(false);
+
+  useEffect(() => {
+   const node = myRef.current; 
+   if (node) {
+     //attach eventListeners
+     node.addEventListener('mouseover', handleMouseOver);
+     node.addEventListener('mouseout', handleMouseOut);
+   }
+    // on dismount disattach
+    return () => {
+      node.removeEventListener('mouseover', handlemOuseOver);
+      node.removeEventListener('mouseout', handleMouseOut);
+    };
+  }, []);
+
+  return [myRef, hovered];
+};
+
+
+export default useHover;
